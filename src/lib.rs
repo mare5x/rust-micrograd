d9bd6a1f47415bda578806ec3e7b7a3a19b2728a
mare5x <mare5x.dev@gmail.com>
2023-03-24 13:52:13 +0100

    Backwards for add.

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,46 @@
-use std::{ops::Add, rc::Rc, cell::{RefCell, Ref}};
+use std::{ops::Add, rc::Rc, cell::{RefCell, Ref, RefMut}};
+use std::fmt;
+
+pub struct GradFn {
+    name: String,
+    grad_fn: Box<dyn FnMut(f64) -> ()>,
+}
+
+impl fmt::Debug for GradFn {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "GradFn[{}]", self.name)
+    }
+}
+
+impl GradFn {
+    fn new(name: &str, grad_fn: impl FnMut(f64) -> () + 'static) -> Self {
+        Self { name: String::from(name), grad_fn: Box::new(grad_fn) }
+    }
+
+    fn empty() -> GradFn {
+        Self::new("None", |_| ())
+    }
+
+    fn call(&mut self, grad: f64) {
+        (self.grad_fn)(grad)
+    }
+}
 
 #[derive(Debug)]
 pub struct Value {
     pub data: f64,
     pub grad: f64,
     prev: Vec<WrappedValue>,
+    grad_fn: GradFn,
 }
 
 impl Value {
     fn new(data: f64) -> Value {
-        Value { data, grad: 0.0, prev: vec![] }
+        Value { data, grad: 0.0, prev: vec![], grad_fn: GradFn::empty() }
     }
 
     fn backward(&mut self) {
-
+        self.grad_fn.call(self.grad)
     }
 }
 
@@ -34,6 +61,11 @@ impl WrappedValue {
         v.borrow()
     }
 
+    fn value_mut(&self) -> RefMut<Value> {
+        let v = &*self.0;
+        v.borrow_mut()
+    }
+
     fn data(&self) -> f64 {
         let v = &*self.0;
         v.borrow().data
@@ -46,11 +78,17 @@ impl Add for WrappedValue {
     fn add(self, rhs: Self) -> Self::Output {
         let v1 = self.clone();
         let v2 = rhs.clone();
-
+        
+        let grad_fn = GradFn::new("add", move |grad| {
+            v1.value_mut().grad += grad;
+            v2.value_mut().grad += grad;
+        });
+        
         let mut v = Value::new(self.data() + rhs.data());
-        v.prev.push(v1);
-        v.prev.push(v2);
-
+        v.prev.push(self.clone());
+        v.prev.push(rhs.clone());
+        v.grad_fn = grad_fn;
+        
         WrappedValue::new(v)
     }
 }
@@ -72,10 +110,18 @@ mod tests {
         let v2 = WrappedValue::from(1.0);
         let v = v1.clone() + v2.clone();
         assert_eq!(v.data(), 6.0);
+        assert_eq!(v.value().grad_fn.name, "add");
 
-        let v = v.value();
-        let prev: Vec<f64> = v.prev.iter().map(|x| x.data()).collect();
-        assert_eq!(prev, vec![v1.data(), v2.data()]);
+        {
+            let val = v.value();
+            let prev: Vec<f64> = val.prev.iter().map(|x| x.data()).collect();
+            assert_eq!(prev, vec![v1.data(), v2.data()]);
+        }
+
+        let mut v = v.value_mut();
+        v.grad = 1.0;
+        v.backward();
+        assert_eq!(v1.value().grad, v.grad);
     }
 
 
